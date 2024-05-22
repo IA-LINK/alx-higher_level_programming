73460232e6df16eece5657ceca3b7e2ed552d93c
jyc.kom@gmail.com <jyc.kom@gmail.com>
2024-05-22 15:16:48 -0700

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,13 +1,9 @@
-# Connect to MySQL as the root user
+-- Connect to MySQL as the root user
 mysql -h localhost -u root -p
+-- Enter your root password when prompted
 
-# Enter your root password when prompted
-
-# Check privileges for user_0d_1
+-- Check privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
-# Check privileges for user_0d_2 (optional, comment out if user doesn't exist)
-#SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
-# Exit MySQL
-quit;
+-- Check privileges for user_0d_2 (optional, comment out if user doesn't exist)
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
