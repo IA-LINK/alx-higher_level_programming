ce4d88d6290565976ce08f93179a4e1afd0d7fdf
jyc.kom@gmail.com <jyc.kom@gmail.com>
2024-05-22 15:22:21 -0700

    fix file

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,4 @@
 -- Connect to MySQL as the root user
-mysql -h localhost -u root -p
--- Enter your root password when prompted
 
--- Check privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Check privileges for user_0d_2 (optional, comment out if user doesn't exist)
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+ SHOW GRANTS FOR 'user_0d_1'@'localhost';
+ SHOW GRANTS FOR 'user_0d_2'@'localhost';
