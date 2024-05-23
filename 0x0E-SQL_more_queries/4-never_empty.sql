9fa4523a9709d044c15b0e57fd6a5b5de84f1da9
jyc.kom@gmail.com <jyc.kom@gmail.com>
2024-05-23 10:57:16 -0700

    re update

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,8 +1,7 @@
 -- Get the database name from the first argument
 -- Create table id_not_null (if not exists)
 
-CREATE TABLE IF NOT EXISTS `@db_name`.`id_not_null` (
-  `id` INT NOT NULL DEFAULT 1,
-  `name` VARCHAR(256),
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS id_not_null (
+  id INT DEFAULT 1,
+  name VARCHAR(256),
 );
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -2,6 +2,6 @@
 -- Create table unique_id (if not exists)
 
 CREATE TABLE IF NOT EXISTS(
-  	INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256),
+  	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
 );
