90771db86f810b2fe3651b5738647610a84a2d50
jyc.kom@gmail.com <jyc.kom@gmail.com>
2024-06-20 16:48:47 -0700

    re-update tasks

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle
-{
-	constructor(w, h)
-	{
-		if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0)
-		{
+module.exports = class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
 			this.width = w;
 			this.height = h;
-		}
-		else
-		{
-			this.width = 0;
-			this.height = 0;
 		}
 	}
 };
