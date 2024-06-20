8e4409d69d51e5ea670c38b59b2fc559efb5ccc8
jyc.kom@gmail.com <jyc.kom@gmail.com>
2024-06-20 16:09:24 -0700

    add tasks

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle
+{
+	constructor(w, h)
+	{
+		if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0)
+		{
+			this.width = w;
+			this.height = h;
+		}
+		else
+		{
+			this.width = 0;
+			this.height = 0;
+		}
+	}
+};
