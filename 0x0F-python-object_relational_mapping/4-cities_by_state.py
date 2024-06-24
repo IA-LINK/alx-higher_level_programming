92b2def71bb4be31b60827505a7333a391e4c1c4
IA-LINK <jyc.kom@gmail.com>
2024-06-24 07:25:23 -0400

    Add 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,38 +1,57 @@
-#!/usr/bin/
-
+#!/usr/bin/python3
 """
-script that lists all cities from the database hbtn_0e_4_usa.
-Parameters for script: mysql username, mysql password, database name.
-Must use the `MySQLdb` module.
-Script should connect to a MySQL server runnimg on `localhost` at port `3306`
-Results must be in ascending order by `cities.id`.
-Can only use `execute()` once
-Code should not be executed when imported.
+a script that lists all cities from the database hbtn_0e_4_usa
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on
+localhost at port 3306
+Results must be sorted in ascending order by cities.id
+You can use only execute() once
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
 """
 
-import MySQLdb
+
+import MySQLdb as mysql
 from sys import argv
 
-if __name__ == "__main__":
-    # establishing a secure connection to the MySQL server
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
 
-    # createing a cursor object to execute SQL queries
-    cursor = db.cursor()
+def connection(connect, username, password, database, prt):
+    """
+        lists all row in states table
+        connect:port,
+        username: username of the user,
+        password: password of the database,
+        database: the database to use,
+        prt: port to connect.
+    """
+    mydb = mysql.connect(
+        host=connect,
+        user=username,
+        passwd=password,
+        db=database,
+        port=prt
+    )
 
-    # executing the cursor to retrieve cities sorted by id
-    city_query = """SELECT cities.id, cities.name, states.name FROM states
-    
-    INNER JOIN cities ON states.id = cities.state_id
-        ORDER BY cities.id ASC"""
-    cursor.execute(city_query)
-    # fetching all the results
-    cities = cursor.fetchall()
-# display/print them out                                                                                                                                                        for city in cities:
-    print(city)
+    # create cursor
+    cursor = mydb.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM cities" \
+            " LEFT JOIN states ON cities.state_id= states.id ORDER" \
+            " BY cities.id ASC"
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # close connections
+    cursor.close()
+    return mydb
+
+
+if __name__ == '__main__':
+    mydbase = connection('localhost', argv[1], argv[2], argv[3], 3306)
+    mydbase.close()
