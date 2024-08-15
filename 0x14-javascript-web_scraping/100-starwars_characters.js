9c73166d53f02e279289fcee972b70e0306bdbf6
ia-link <jyc.kom@gmail.com>
2024-08-15 01:06:55 -0700

    fix advance tasks

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,37 +1,21 @@
 #!/usr/bin/node
 
-// Require the 'request' module
-const request = require('request');
-
-// Get movieID from command line args
-const movieId = process.argv[2];
-// Get the URl for SWAPI film API
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-// Make request to the SWAPI film API
-request(apiUrl, (error, response, body) => {
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
   if (error) {
-    // check for errors in the request
-    console.error(error);
-  } else if (response.statusCode === 200) {
-    // if statusCode is 200 (OK), proceed
-
-    const movieData = JSON.parse(body); // Parse the JSON body response
-
-    // Iterate over each character in the movie
-    for (const characterURL of movieData.characters) {
-      // Make another request to fetch info on each characters
-      request(characterURL, (error, response, charBody) => {
-        // Check for errors in the request for character details
-        if (error) {
-          console.log(error);
-        } else if (response.statusCode === 200) {
-          console.log(JSON.parse(charBody).name);
-        }
-      });
-    }
-  } else {
-    // if response statusCode is not 200, output error message
-    console.log('Error code: ' + response.statusCode);
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
   }
 });
