c817fe1e0251b369d042e4a8c93d994e0a436781
ia-link <jyc.kom@gmail.com>
2024-08-15 00:48:48 -0700

    fix task 0 and 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,14 @@
-#!/usr/bin/node
-
+#!/usr/bin/env node
 const fs = require('fs');
+
+if (process.argv.length !== 3) {
+    console.error("Usage: ./0-readme.js <file_path>");
+    process.exit(1); 
+}
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', function(err, data){
+fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
     } else {
