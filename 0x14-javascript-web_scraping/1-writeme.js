c68774fa0d50c90eb797e81f17e8397d7240feac
ia-link <jyc.kom@gmail.com>
2024-08-15 00:52:55 -0700

    fix tasks 0 and 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,5 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-    console.error("Usage: ./1-writeme.js <file_path> <string_to_write>");
-    process.exit(1); 
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf8', (err) => {
-    if (err) {
-        console.error(err);
-    } 
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
